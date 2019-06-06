51d3d9dd1002d7b47a0642de24e86f2202dc6d7a
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2019-06-05 23:29:38 -0500

    basic vector creation

---
+++ vectors_and_keywords.clj
@@ -0,0 +1,4 @@
+:my_keyword ; esto es una palabra clave (? átomo en elixir)
+(def my_vector [:uno :dos :tres]) ; esto es una variable
+(println my_vector)  ; ¿Qué pasa si lo intentamos imprimir?
+(println (first my_vector))
