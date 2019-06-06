7f8296e221cafca4f72342ac5b13d400728bb3e9
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2019-06-05 23:46:28 -0500

    experimentando con read-string y listas

--- vectors_and_keywords.clj
+++ vectors_and_keywords.clj
@@ -2,3 +2,13 @@
 (def my_vector [:uno :dos :tres]) ; esto es una variable
 (println my_vector)  ; ¿Qué pasa si lo intentamos imprimir?
 (println (first my_vector))
+(println (last my_vector))  ; ? Existe last
+
+
+; expetimentando con readstring
+
+(def read_vector (read-string "[:one :two :three]"))
+(println read_vector)
+
+(def my-list (list :an :du :trois))  ; !!! Los nombres soportan guíones medios
+(println my-list)
